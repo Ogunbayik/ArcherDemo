bf5d297fbb3ab8ffa19f1f16be88f95624c9ac0f
Ogunbayik <ogun.bayikk@gmail.com>
2025-11-28 00:03:46 +0300

    Add PlayerController and Add GameConstant

---
+++ Assets/Scripts/GameConstant.cs
@@ -0,0 +1,10 @@
+public static class GameConstant
+{
+    public static class PlayerInput
+    {
+        public const string HORIZONTAL_INPUT = "Horizontal";
+        public const string VERTICAL_INPUT = "Vertical";
+    }
+
+
+}
